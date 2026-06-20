777ff8a27d7bb809cc3ca7e31dfe077d779d1102
Emma Gabriel <emmaviolet@github.com>
2026-06-20 20:18:06 +0100

    Modify site constants for new branding and socials
    Updated site title and description, and added social links.

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,11 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'emmaviolet\'s blog';
+export const SITE_DESCRIPTION = 'A collection of thoughts on things';
+
+export const SOCIALS = {
+  x: 'https://x.com/emmaviolet',
+  github: 'https://github.com/emmaviolet',
+  linkedin: 'https://www.linkedin.com/in/emma-gabriel',
+};
